cd4c04814c4163990ea78c902729ca52e5ad0db7
exchris <2687547643@qq.com>
2018-08-15 10:47:48 +0800

    阶乘函数

--- Mysql/factorial.sql
+++ Mysql/factorial.sql
@@ -1,13 +1,11 @@
--- 阶乘函数
-DROP FUNCTION IF EXISTS Factorial;
 -- 创建阶乘函数
-CREATE FUNCTION Factorial(n INT(11)) RETURNS INT(11) COMMENT '阶乘函数'
+CREATE FUNCTION Factorial(n INT) RETURNS INT 
 BEGIN
-  DECLARE i INT DEFAULT 1;
-  DECLARE result INT DEFAULT 1;
-  WHILE i <= n DO 
-    SET result = result * i;
-    set i = i + 1;
+  DECLARE i int DEFAULT 1;
+  DECLARE result int DEFAULT 1;
+  WHILE i<=n DO
+    SET result = result*i;
+    SET i=i+1;
   END WHILE;
   RETURN result;
 END
