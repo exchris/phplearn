84c61460d6aff929d138ff8a7f1ab72910cb2882
exchris <dev.erxuan@gmail.com>
2018-11-30 09:58:07 +0800

    array shuffle

---
+++ Basic/Array/shuffle_demo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/30
+ * Time: 9:54
+ * @author exchris
+ */
+
+$my_array = array(
+    "a" => "red",
+    "b" => "green",
+    "c" => "blue",
+    "d" => "yellow",
+    "e" => "purple"
+);
+
+shuffle($my_array);
+
+print_r($my_array);
+
