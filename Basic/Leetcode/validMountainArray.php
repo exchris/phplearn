a132938928c30dc3e0753561c8899568479ca277
exchris <dev.erxuan@gmail.com>
2019-02-25 16:31:46 +0800

    leetcode 有效的山峰数组

---
+++ Basic/Leetcode/validMountainArray.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 941.有效的山脉数组
+ *
+ *
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/4
+ * Time: 10:39
+ * @author exchris
+ */
+function validMountainArray($arr)
+{
+    return [];
+}
